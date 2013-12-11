15c12c0a3ce6a4ba5f5af29520a7f3e61fcbf6f5
James Wu <james.wu@students.olin.edu>
2013-12-10 21:32:58 -0500

    trying things

--- Transmitter.m
+++ Transmitter.m
@@ -1,8 +1,8 @@
 function res = Transmitter()
     addpath USRP_Tools\
     n = 5000;
-    I = ones(n,1);
-    Q = ones(n,1);
+    I = ones(n,1).*10;
+    Q = ones(n,1).*10;
     while 1
         USRP_SendSamples(I, Q, n)
     end
