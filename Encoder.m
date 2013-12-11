c6de14a923f908bf8cca7414eab6a827fba30ecc
lisapark <lisapark.bke@gmail.com>
2013-12-10 23:18:02 -0500

    Basic Encoder, Protocol done. Created Decoder

--- Encoder.m
+++ Encoder.m
@@ -1,10 +1,36 @@
-function encodedMessage = Encoder(message)
+%input user, and message (perhaps just binary for now)
 
-    encodedMessage = encodeCDMA(message);
+%functions: stringToBinary, encodeBinary,binaryToVolt
+
+function encodedMessage = Encoder(user,message)
+%user is string, message is double
+    
+    encodedMessage = encodeCDMA(user,message);
 
 end
 
-function encodedMessage = encodeCDMA(string)
-    protocol = Protocol;
-    encodedMessage = dec2bin(string)
+function encodedMessage = encodeCDMA(user,string)
+    protocol = Protocol();
+    binaryMessage = dec2bin(string);
+    binaryMessage = binaryToVolt(binaryMessage);
+    userCode = protocol(user);
+    encodedMessage = zeros(1,length(binaryMessage)*length(userCode));
+    bitPlace = 0;
+    for i = 1: length(binaryMessage)
+        for j = 1:length(userCode)
+            bitPlace = bitPlace + 1;
+            encodedMessage(bitPlace) = binaryMessage(i)*userCode(j)
+        end
+    end
+end
+
+function binaryMessage = binaryToVolt(trueBinary)
+    binaryMessage = zeros(1, length(trueBinary))
+    for i = 1:length(trueBinary)
+        if str2double(trueBinary(i)) == 0
+            binaryMessage(i) = -1
+        else
+            binaryMessage(i) = 1
+        end
+    end
 end
