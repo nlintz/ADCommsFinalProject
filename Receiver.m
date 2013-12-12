651cb4e8906ae722d74215da7be470d0011d50ed
Nathan Lintz <Nlintz@gmail.com>
2013-12-11 21:24:20 -0500

    added figures

--- Receiver.m
+++ Receiver.m
@@ -17,16 +17,16 @@ classdef Receiver<handle
             obj.Z = I + 1i.*Q;
         end
         
-        function obj=filterPeak(obj, fd)
-            filtered_z = transpose(obj.Z).*exp(-1i*fd*linspace(1,length(obj.Z),length(obj.Z)));
-            subplot(2, 1, 1)
-            plot(real(filtered_z))
-            subplot(2, 1, 2)
-            plot(real(obj.Z))
-            obj.Z = transpose(filtered_z);
-            obj.I = real(obj.Z);
-            obj.Z = imag(obj.Z);
-        end        
+%         function obj=filterPeak(obj, fd)
+%             filtered_z = transpose(obj.Z).*exp(-1i*fd*linspace(1,length(obj.Z),length(obj.Z)));
+%             subplot(2, 1, 1)
+%             plot(real(filtered_z))
+%             subplot(2, 1, 2)
+%             plot(real(obj.Z))
+%             obj.Z = transpose(filtered_z);
+%             obj.I = real(obj.Z);
+%             obj.Z = imag(obj.Z);
+%         end        
         function plotFFT(obj, signal)
             clf
             plot(linspace(-pi,pi,length(signal)),abs(fftshift(fft(signal))))
@@ -36,7 +36,20 @@ classdef Receiver<handle
     
     methods(Static)
         function peak = findPeak(signal)
-            peak = signal;
+            fft_result = abs(fftshift(fft(signal)));
+            [M I] = max(fft_result);
+            
+            peak = abs(2*pi*(I/8000 - 1/2));
+        end
+    end
+    methods(Static)
+        function filteredZ = filterPeak(unfilteredZ, fd)
+            filtered_z = transpose(unfilteredZ).*exp(-1i*fd*linspace(1,length(unfilteredZ),length(unfilteredZ)));
+            subplot(2, 1, 1)
+            plot(real(filtered_z))
+            subplot(2, 1, 2)
+            plot(real(obj.Z))
+            filteredZ = transpose(filtered_z);
         end
     end
     
