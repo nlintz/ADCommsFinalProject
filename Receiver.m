8df48f3578032cbf7b55ebff9b87baba34f7bb72
James Wu <james.wu@students.olin.edu>
2013-12-11 20:46:46 -0500

    attempt at receiver constructor

--- Receiver.m
+++ Receiver.m
@@ -1,15 +1,29 @@
 classdef Receiver<handle
     %UNTITLED7 Summary of this class goes here
     %   Detailed explanation goes here
-    properties
-        I = []
-        Q = []
-        Z = []
-        data = [];
-        peakFrequency;
+%     properties (SetAccess = private)
+%         I = []
+%         Q = []
+%         Z = []
+%         data = [];
+%         peakFrequency;
+%     end
+    
+    properties (SetAccess = private)
+        I
+        Q
+        Z
+        data
+        peakFrequency
     end
     
     methods
+        function obj=Receiver(I, Q)
+            obj.I = I;
+            obj.Q = Q;
+            obj.Z = I + 1i.*Q;
+        end
+        
         function obj=receieveData(obj, packetLength)
             addpath USRP_Tools\;
             obj.data = USRP_RxPacket(packetLength, 50, 10, 100);
