0bb0279eb7b94222638b095dd26c1d16eb2ec452
Nathan Lintz <Nlintz@gmail.com>
2013-12-11 19:33:50 -0500

    changed receiever to a class

--- Receiver.m
+++ Receiver.m
@@ -1,5 +1,19 @@
-function res = Receiver(packetLength)
-    addpath USRP_Tools\;
-    [I Q] = USRP_RxPacket(packetLength, 50, 10, 100);
-    res = [I Q];
+classdef Receiver<handle
+    %UNTITLED7 Summary of this class goes here
+    %   Detailed explanation goes here
+    properties
+        I = []
+        Q = []
+    end
+    
+    methods
+        function obj=receieveData(obj, packetLength)
+            addpath USRP_Tools\;
+            [I Q] = USRP_RxPacket(packetLength, 50, 10, 100);
+            obj.I = I;
+            obj.Q = Q;
+        end
+    end
+    
 end
+
