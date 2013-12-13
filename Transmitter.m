7a6da9e0e014f13ba8ba239d1ac7419d7d9cb8b4
James Wu <james.wu@students.olin.edu>
2013-12-13 15:03:29 -0500

    transmit constellation points

--- Transmitter.m
+++ Transmitter.m
@@ -1,14 +1,14 @@
 function res = Transmitter()
     addpath USRP_Tools\
-    n = 5000;
-    I = ones(n,1).*10;
-    Q = ones(n,1).*10;
+    n = 2000;
+    I = ones(n,1).*15;
+    Q = ones(n,1).*15;
     
-    TransmitI = vertcat(I, -1.*I);
-    TransmitQ = vertcat(Q, -1.*Q);
+    TransmitI = vertcat(I,I, I, -1.*I, -1.*I);
+    TransmitQ = vertcat(Q,Q,-1.*Q,Q,-1.*Q);
 
     while 1
-        USRP_SendSamples(I, Q, n)
-        %USRP_SendSamples(TransmitI, TransmitQ, n*2)
+        %USRP_SendSamples(I, Q, n)
+        USRP_SendSamples(TransmitI, TransmitQ, n*5)
     end
 end
