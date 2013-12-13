445963fd8545b1ca1d7821a528e6d0164b325e15
James Wu <james.wu@students.olin.edu>
2013-12-13 15:43:58 -0500

    transmit 3 constellation points

--- Transmitter.m
+++ Transmitter.m
@@ -4,11 +4,11 @@ function res = Transmitter()
     I = ones(n,1).*15;
     Q = ones(n,1).*15;
     
-    TransmitI = vertcat(I,I, I, -1.*I, -1.*I);
-    TransmitQ = vertcat(Q,Q,-1.*Q,Q,-1.*Q);
+    TransmitI = vertcat(I,I, I, -1.*I);
+    TransmitQ = vertcat(Q,Q,-1.*Q,Q);
 
     while 1
         %USRP_SendSamples(I, Q, n)
-        USRP_SendSamples(TransmitI, TransmitQ, n*5)
+        USRP_SendSamples(TransmitI, TransmitQ, n*4)
     end
 end
