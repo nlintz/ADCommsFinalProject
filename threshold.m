fc629501c6373cf45b534b989c32e2e641e55b34
James Wu <james.wu@students.olin.edu>
2013-12-12 22:48:09 -0500

    threshold function

---
+++ threshold.m
@@ -0,0 +1,12 @@
+function res = threshold(data)
+%data = 2*rand(50,1)-1;
+datath = zeros(1,length(data));
+for i=1:length(data)
+    if data(i) < 0
+        datath(i) = 0;
+    else
+        datath(i) = 1;
+    end
+end
+res = datath;
+end
