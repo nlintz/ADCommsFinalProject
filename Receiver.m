21e7bc07a9125970a8d485d41f056359e6d5cb5b
Nathan Lintz <Nlintz@gmail.com>
2013-12-12 21:41:46 -0500

    figured out how to correct frequency and find peaks

--- Receiver.m
+++ Receiver.m
@@ -13,35 +13,25 @@ classdef Receiver < handle
         
         function obj=receieveData(obj, packetLength)
             addpath USRP_Tools\;
-            [I Q] = USRP_RxPacket(packetLength, 50, 10, 100);
+            [I Q] = USRP_RxPacket(packetLength, 50, 15, 100);
             obj.I = I;
             obj.Q = Q;
             obj.Z = I + 1i.*Q;
         end
         
-%         function obj=filterPeak(obj, fd)
-%             filtered_z = transpose(obj.Z).*exp(-1i*fd*linspace(1,length(obj.Z),length(obj.Z)));
-%             subplot(2, 1, 1)
-%             plot(real(filtered_z))
-%             subplot(2, 1, 2)
-%             plot(real(obj.Z))
-%             obj.Z = transpose(filtered_z);
-%             obj.I = real(obj.Z);
-%             obj.Z = imag(obj.Z);
-%         end        
-        function plotFFT(obj, signal)
+        function showFFT(obj)
             clf
-            plot(linspace(-pi,pi,length(signal)),abs(fftshift(fft(signal))))
+            plot(linspace(-pi,pi,length(obj.I)),abs(fftshift(fft(obj.I))))
         end
         
     end
     
     methods(Static)
         function peak = findPeak(signal)
+            x = linspace(-pi, pi, length(signal));
             fft_result = abs(fftshift(fft(signal)));
             [M index] = max(fft_result);
-            
-            peak = abs(2*pi*(index/8000 - 1/2));
+            peak = x(index);
         end
     end
     methods(Static)
@@ -49,9 +39,16 @@ classdef Receiver < handle
             filtered_z = transpose(unfilteredZ).*exp(-1i*fd*linspace(1,length(unfilteredZ),length(unfilteredZ)));
             subplot(2, 1, 1)
             plot(real(filtered_z))
+            subplot(2, 1, 2)
+            plot(real(filtered_z), imag(filtered_z));
             filteredZ = transpose(filtered_z);
         end
     end
     
+    methods(Static)
+        function plotFFT(signal)
+            plot(linspace(-pi,pi,length(signal)),abs(fftshift(fft(signal))));
+        end
+    end
+    
 end
-
