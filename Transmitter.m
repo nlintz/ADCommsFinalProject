53c64e44a926c81c6aa22e888ad205d0648102c5
lisapark <lisapark.bke@gmail.com>
2013-12-14 15:44:23 -0500

    Decoder padding zeros out. encoder n=100

--- Transmitter.m
+++ Transmitter.m
@@ -1,11 +1,11 @@
 function res = Transmitter()
     addpath USRP_Tools\
     n = 2000;
-    I = ones(n,1).*15;
-    Q = ones(n,1).*15;
+    I = ones(n,1).*10;
+    Q = ones(n,1).*10;
     
-    TransmitI = vertcat(I,I, I, -1.*I);
-    TransmitQ = vertcat(Q,Q,-1.*Q,Q);
+    TransmitI = vertcat(I,I, -1.*I, -1.*I);
+    TransmitQ = vertcat(Q,-1.*Q,Q, -1.*Q);
 
     while 1
         %USRP_SendSamples(I, Q, n)
