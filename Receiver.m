fe9bc3399da7cfd19cd2f9821860d65b734a5e6b
Nathan Lintz <Nlintz@gmail.com>
2013-12-14 15:42:50 -0500

    added data

--- Receiver.m
+++ Receiver.m
@@ -13,7 +13,7 @@ classdef Receiver < handle
         
         function obj=receieveData(obj, packetLength)
             addpath USRP_Tools\;
-            [I Q] = USRP_RxPacket(packetLength, 50, 5, 50);
+            [I Q] = USRP_RxPacket(packetLength, 50, 15, 50);
             obj.I = I;
             obj.Q = Q;
             obj.Z = I + 1i.*Q;
