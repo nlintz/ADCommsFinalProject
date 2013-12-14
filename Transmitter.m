e4d5ea0e43922d7c65a05b58c077696c8de95838
lisapark <lisapark.bke@gmail.com>
2013-12-14 16:55:24 -0500

    decoder contractMessage adjust

--- Transmitter.m
+++ Transmitter.m
@@ -1,28 +1,24 @@
 function res = Transmitter(user, message)
     addpath USRP_Tools\
     
-    encodedMessage = Encoder(user, message);
-    
-    n = length(encodedMessage);
-    I = [];
-    Q = [];
-    for i = 1: n
-        if rem(i,2) == 1
-            I = cat(I,encodedMessage(i));
-        else
-            Q = cat(Q,encodedMessage(i));
-        end
-    end
+    encodedMessage = transpose(Encoder(user, message));
     
     %TransmitI = vertcat(I,I, -1.*I, -1.*I);
     %TransmitQ = vertcat(Q,-1.*Q,Q, -1.*Q);
     
-    TransmitI = vertcat(ones(2000,1),I);
-    size(TransmitI)
-    TransmitQ = vertcat(ones(2000,1),Q);
+    I = encodedMessage;
+    Q = encodedMessage;
+%     encodedMessage
+%     ones(2000,1)
+%     TransmitI = vertcat(ones(2000,1),I)
+    I = ones(2000,1)
+    TransmitI = vertcat(I, -1.*I,I, -1.*I);
+    TransmitQ = TransmitI;
+    size(encodedMessage)
+%     TransmitQ = vertcat(ones(2000,1),Q);
 
     while 1
         %USRP_SendSamples(I, Q, n)
-        USRP_SendSamples(TransmitI, TransmitQ, n*4)
+        USRP_SendSamples(TransmitI, TransmitQ, length(TransmitI))
     end
 end
